beb3d23c52077a0909149c5589f8b7e3317dfcdd
Thomas-Casey <102041956@student.swin.edu.au>
2021-04-19 03:00:28 +1000

    first3

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace listsTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numbers;
+            int numberi;
+            int counter1 = 1;
+
+            List<int> Numlst = new List<int>();
+
+            while (counter1 < 6)
+            {
+                Console.WriteLine("Enter number " + counter1 + ":");
+                numbers = Console.ReadLine();
+                numberi = Convert.ToInt32(numbers);
+                Numlst.Add(numberi);
+                counter1++;
+            }
+            Console.WriteLine("Enter Another Number:");
+            string x;
+            int xi;
+
+            x = Console.ReadLine();
+            xi = Convert.ToInt32(x);
+            if {
+                List<int>.Contains(xi)
+                Console.WriteLine("number allready exists.")
+        }
+            else
+
+
+
+        }
+    }
+}
+
+
